6040dd02af1c8aa830adb9fbd0e399c6078b71b4
im.hossain <im.hossain@blmanagedservices.com>
2024-03-31 09:12:51 +0600

    quick sort added

--- src/main/java/org/example/SortingAlgorithms/Main.java
+++ src/main/java/org/example/SortingAlgorithms/Main.java
@@ -2,15 +2,17 @@ package org.example.SortingAlgorithms;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[]{12, 32, 11, 23, 7, 89, 36, 21, 44, 75, 39};
-//        numbers = new int[]{6, 9, 12, 2, 5, 78, 3, 56, 7, 90, 54, 76, 89, 87, 90, 865, 43, 21};
+        int[] numbers = new int[]{12, 32, 11, 23, 7, 89, 36, 21, 44, 75, 20};
+        numbers = new int[]{6, 9, 12, 2, 5, 78, 3, 56, 7, 90, 54, 76, 89, 87, 90, 865, 43, 21};
 //        BubbleSort bubble = new BubbleSort(numbers);
 //        InsertionSort insertion = new InsertionSort(numbers);
-        SelectionSort selection = new SelectionSort(numbers);
+//        SelectionSort selection = new SelectionSort(numbers);
+        QuickSort quick = new QuickSort(numbers);
 //
-//        int[] result = bs.sort();
-//        int[] result = is.sort();
-        int[] result = selection.sort();
+//        int[] result = bubble.sort();
+//        int[] result = insertion.sort();
+//        int[] result = selection.sort();
+        int[] result = quick.sort();
         for (int number : result) {
             System.out.println(number);
         }
---
+++ src/main/java/org/example/SortingAlgorithms/QuickSort.java
@@ -0,0 +1,41 @@
+package org.example.SortingAlgorithms;
+
+public class QuickSort implements Sort {
+    private int[] numbers;
+
+    QuickSort(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public void partition(int start, int end) {
+        int pivot = numbers[end];
+        int current = 0;
+        for (int i = 0; i < end; i++) {
+            if (numbers[i] <= pivot) {
+                swap(current, i);
+                current++;
+            }
+        }
+        swap(current, end);
+        if (start < current - 1) {
+            partition(start, current - 1);
+        }
+        if (current + 1 < end) {
+            partition(current + 1, end);
+        }
+    }
+
+    public void swap(int i, int j) {
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
+
+    //{12, 32, 11, 23, 7, 89, 36, 21, 44, 75, 20}
+    @Override
+    public int[] sort() {
+        int end = numbers.length - 1;
+        partition(0, end);
+        return numbers;
+    }
+}
