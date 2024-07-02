73abc4bf212760175c2e4b46077adb4b5969d11e
mdimran1409036 <mdimran1409036@gmail.com>
2024-07-02 07:44:42 +0600

    stack data structure added

---
+++ src/main/java/org/example/DataStructure/stack/Stack.java
@@ -0,0 +1,65 @@
+package org.example.DataStructure.stack;
+
+import java.util.Arrays;
+
+public class Stack {
+    private final int capacity;
+    private int[] stack;
+    private int top;
+    Stack(int capacity){
+        this.top =-1;
+        this.capacity=capacity;
+        this.stack = new int[capacity];
+    }
+    public void push(int num){
+        //check if stack is full
+        if(this.isFull()){
+            System.out.println("The Stack is full, i.e. Stack Overflow, can't add");
+            return;
+        }
+        this.top += 1;
+        stack[this.top] = num;
+    }
+    public int pop(){
+        if(this.isEmpty()){
+            System.out.println("The stack is empty, i.e. Stack Underflow, can't pop");
+        }
+        return stack[top--]; //post decrement
+    }
+    public int top(){
+        if(this.isEmpty()){
+            System.out.println("The stack is empty");
+        }
+        return stack[top];
+    }
+    public boolean isEmpty(){
+        return this.top == -1;
+    }
+    public boolean isFull(){
+        return this.capacity-1 == this.top;
+    }
+
+    @Override
+    public String toString() {
+        return "Stack{" +
+                "capacity=" + capacity +
+                ", stack=" + Arrays.toString(stack) +
+                ", top=" + top +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        System.out.println("popped element:" + stack.pop());
+        System.out.println("is stack empty:" + stack.isEmpty());
+        stack.push(60);
+        System.out.println("current top element:"+ stack.top());
+        System.out.println("is stack full:"  + stack.isFull());
+        System.out.println(stack);
+    }
+}
